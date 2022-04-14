2971561e9240582ac009043edcfdceda053d5036
Lilian Li <lil712@hotmail.com>
2022-04-14 17:02:43 +0800

    watch-list

---
+++ app/models/list.rb
@@ -0,0 +1,6 @@
+class List < ApplicationRecord
+  validates :name, uniqueness: true
+  has_many :bookmarks, dependent: :destroy
+  validates :name, presence: true
+  has_many :movies, through: :bookmarks
+end
